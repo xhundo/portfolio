08c6749f377877225f209fb6398f1e75da5f65ef
Kurtney Joseph <josephkurt18@gmail.com>
2023-03-21 01:36:48 -0400

    refactor: index script build

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import { template } from "../utils/constants";
+import { skillTemp } from "../utils/constants";
+import { skillSet } from "../utils/constants";
+import html5 from "../assets/mobile/html5.svg";
+import css from "../assets/mobile/CSS3.svg";
+import js from "../assets/mobile/JS.svg";
+import express from "../assets/mobile/express.svg";
+import node from "../assets/mobile/NODE.svg";
+import react from "../assets/mobile/REACT.svg";
+import mongo from "../assets/mobile/MONGO.svg";
+import github from "../assets/mobile/Github-m.svg";
+import figma from "../assets/mobile/figma-logo-1.svg";
 
 let elems;
+let skills2;
+let skill;
 const container = document.querySelector("#projects");
+const wrapper = document.querySelector("#skills");
+const container2 = document.querySelector("#skills-3");
+const wrapper2 = document.querySelector("#skills-2");
 const backward = document.querySelector("#backward");
 const forward = document.querySelector("#forward");
+let newSkills;
 
 const projects = [
     {
@@ -25,11 +42,59 @@ const projects = [
         link: "https://github.com/xhundo",
     },
     {
-        title: "",
-        src: "",
+        title: "#",
+        src: "#",
         alt: "Project 004",
         link: "https://github.com/xhundo",
     },
+];
+
+const mobileSkills = [
+    {
+        icon: html5,
+        skill: "HTML",
+        alt: "HTML5 skill icon",
+    },
+    {
+        icon: css,
+        skill: "CSS3",
+        alt: "CSS3 skill icon",
+    },
+    {
+        icon: js,
+        skill: "JS",
+        alt: "JavaScript skill icon",
+    },
+    {
+        icon: express,
+        alt: "Express.js skill icon",
+    },
+    {
+        icon: node,
+        skill: "Node",
+        alt: "Node.js Skill Icon",
+    },
+    {
+        icon: react,
+        skill: "React",
+        alt: "React.js Skill Icon",
+    },
+    {
+        icon: mongo,
+        skill: "Mongo",
+        alt: "MongoDB Skill Icon",
+    },
+    {
+        icon: github,
+        skill: "GitHub",
+        alt: "Github Skill Icon",
+    },
+    {
+        icon: figma,
+        skill: "Figma",
+        alt: "Figma Skill Icon",
+    },
+    {},
 ];
 
 function getContent(item) {
@@ -47,10 +112,35 @@ function getContent(item) {
     return elems;
 }
 
+function getSkills(i) {
+    skill = skillTemp.content.cloneNode(true);
+    const skills = skillTemp.content;
+    const skillIcon = skills.querySelector("#icon");
+    const skillName = skills.querySelector("#skill-p");
+    const skillWrapper = skills.querySelector("#skill-container");
+    skillIcon.src = i.icon;
+    skillIcon.alt = i.alt;
+
+    if (i.hasOwnProperty("skill")) {
+        skillWrapper.classList.add("gap-x-2");
+        skillName.textContent = i.skill;
+    } else {
+        skillWrapper.classList.remove("gap-x-2");
+        skillName.textContent = "";
+    }
+
+    return skill;
+}
+
 function renderer(items, container, cb) {
     items.forEach((i) => {
-        cb(i);
-        container.append(elems);
+        if (items === projects && container === container) {
+            cb(i);
+            container.append(elems);
+        } else {
+            cb(i);
+            container.append(skill);
+        }
     });
 }
 
@@ -64,5 +154,10 @@ if (projects.length > 4) {
     backward.setAttribute("disabled", "");
 }
 
-console.log(backward, forward);
+newSkills = mobileSkills.slice(0, 6);
+skills2 = mobileSkills.slice(5, 10);
+
 renderer(projects, container, getContent);
+renderer(newSkills, wrapper, getSkills);
+renderer(skills2, wrapper2, getSkills);
+renderer(skillSet, container2, getSkills);
