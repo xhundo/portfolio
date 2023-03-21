ffdaf5b21ec55fdb3fd1e9f18dc53bf0e9846dc4
Kurtney Joseph <josephkurt18@gmail.com>
2023-03-21 01:35:01 -0400

    feat: add constant

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,60 @@
-export const template = document.getElementById('template');
+export const template = document.getElementById("template");
+export const skillTemp = document.getElementById("skill");
+import html5 from "../assets/desktop/HTML5-d.svg";
+import css3 from "../assets/desktop/CSS3-d.svg";
+import js from "../assets/desktop/JS-d.svg";
+import node from "../assets/desktop/NODE-d.svg";
+import express from "../assets/desktop/express-d.svg";
+import react from "../assets/desktop/REACT-d.svg";
+import mongo from "../assets/desktop/MONGO-d.svg";
+import github from "../assets/desktop/Github-d.svg";
+import figma from "../assets/desktop/Figma-logo-d.svg";
+
+export const skillSet = [
+    {
+        icon: html5,
+        skill: "HTML5",
+        alt: "HTML5 skill icon",
+    },
+    {
+        icon: css3,
+        skill: "CSS3",
+        alt: "CSS3 skill icon",
+    },
+    {
+        icon: js,
+        skill: "JS",
+        alt: "JavaScript skill icon",
+    },
+
+    {
+        icon: node,
+        skill: "Node",
+        alt: "Node.js Skill Icon",
+    },
+    {
+        icon: express,
+        alt: "Express.js skill icon",
+    },
+    {
+        icon: react,
+        skill: "React",
+        alt: "React.js Skill Icon",
+    },
+    {
+        icon: mongo,
+        skill: "Mongo",
+        alt: "MongoDB Skill Icon",
+    },
+    {
+        icon: github,
+        skill: "GitHub",
+        alt: "Github Skill Icon",
+    },
+    {
+        icon: figma,
+        skill: "Figma",
+        alt: "Figma Skill Icon",
+    },
+    {},
+];
