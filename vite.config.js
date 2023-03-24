f8bdd253d5ab9b531acc2664d68f663837c6a21a
Kurtney Joseph <josephkurt18@gmail.com>
2023-03-24 18:08:42 -0400

    refactor: build ready

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: './src/index.html',
-  },
+    server: {
+        port: 3000,
+        open: "./src/index.html",
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+            },
+        },
+        watch: {
+            exclude: "node_modules/**",
+            include: "css/**",
+        },
+    },
 });
